cc0deef5c7c9801e1574fcaf7f3efc69b76fd463
Runjini <runjini_murthy@hotmail.com>
2018-01-17 20:07:03 -1000

    Further cleanup

--- WV-Facebook-Script-for-repo.R
+++ WV-Facebook-Script-for-repo.R
@@ -7,7 +7,7 @@ library("Rfacebook")
 
 # In order to access data for either your individual Facebook account or data from a public page, a token must be used to access Facebook.  This assumes you have set up your account accordingly on the Facebook Developer page.
 
-myouath <- fbOAuth(app_id='id',app_secret='id')
+myouath <- fbOAuth(app_id='1395144527269024',app_secret='5a0d214a42d350b3cec29de597d82d6b')
 save(myouath, file="myoauth")
 load("myoauth")
 me <- getUsers("me", token = myouath)
@@ -17,10 +17,6 @@ me$name
 
 getpagedata <- getPage (310668279073583, token = myouath, n = 250)
 
-# View getpage data in R Studio.
-
-View(getpagedata)
-
 # Write the dataset to a file on your local machine.
 
 write.csv(getpagedata, "/Users/runjini.murthy/Desktop/GitHub/Runjini_Sprint_4/last-250-wv-fb-posts-Rfacebook.csv")
@@ -28,10 +24,6 @@ write.csv(getpagedata, "/Users/runjini.murthy/Desktop/GitHub/Runjini_Sprint_4/la
 # Read the newly created .CSV dataset in R
 
 WVFBData <- read.csv(file="/Users/runjini.murthy/Desktop/GitHub/Runjini_Sprint_4/last-250-wv-fb-posts-Rfacebook.csv", header=TRUE, sep=",")
-
-# View the WVFBData dataset
-
-View(WVFBData)
 
 # Create a "total engagements" column, that combines likes plus comments plus shares.
 
@@ -45,19 +37,6 @@ library(parsedate)
 # Create a new column in dataset that reformats the date and time from the created_time column.
 
 WVFBData$parsed_timestamp <- parse_iso_8601(WVFBData$created_time)
-View (WVFBData)
-
-# Install chron package for date/time column split.  (THIS MIGHT NOT BE NECESSARY.)
-
-install.packages("chron")
-library(chron)
-
-# Parse the created_time column into the date, time, and the singular posting hour for further analysis.  View to see new columns.
-
-WVFBData$parsed_date <- format(WVFBData$parsed_timestamp, "%m/%d/%y")
-WVFBData$parsed_time <- format(WVFBData$parsed_timestamp, "%H:%M")
-WVFBData$post_hour <- format(WVFBData$parsed_timestamp, "%H")
-View (WVFBData)
 
 # Realized that the timestamps were in the incorrect timezone, so adjusted the columns and created a time-adjusted timestamp.  To adjust the timestamp, you have to add or subtract seconds, as the timestamp is based on the seconds elapsed since 1970.
 
@@ -65,24 +44,9 @@ WVFBData$adjusted_timestamp <- WVFBData$parsed_timestamp - 10*60*60
 WVFBData$parsed_date <- format(WVFBData$adjusted_timestamp, "%m/%d/%y")
 WVFBData$parsed_time <- format(WVFBData$adjusted_timestamp, "%H:%M")
 WVFBData$post_hour <- format(WVFBData$adjusted_timestamp, "%H")
-iew(WVFBData)
+View(WVFBData)
 write.csv(WVFBData, "/Users/runjini.murthy/Desktop/GitHub/Runjini_Sprint_4/last-250-wv-fb-posts-Rfacebook.csv")
 
-# For filtering, install dplyr package.
-
-install.packages("dplyr")
-library(dplyr)
-
-# As an example, create a filter of the dataset that has only those posts from the midnight hour.
-
-twelveam <- filter(WVFBData, post_hour == "00")
-
-# Test out calculations using mean function
-
-mean(twelveam$likes_count)
-mean(twelveam$shares_count)
-mean(twelveam$comments_count)
-
-# Plot average of total engagements column for each post hour (THIS IS NOT CORRECT; SEEMS TO BE SHOWING SUM RATHER THAN MEAN)
+# Plot average of total engagements column for each post hour.
 
 ggplot(WVFBData) + geom_bar(aes(x=post_hour,y=total_engagement),stat="summary", fun.y = "mean",fill=I("grey50"))
